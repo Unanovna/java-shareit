9b0442dc10f1595e5f8f337bc03ade45a4a4e81d
Анна Шалтайкина <>
2024-07-12 18:03:06 +0300

    sprint14

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -112,16 +112,15 @@ public class BookingServiceImpl implements BookingService {
             throw new InternalServerError(String.format("For booking with id: %s Owner is not installed!", booker.getId()));
         }
         Long ownerId = owner.getId();
-        if (accessForBooker) {
-            if (!bookerId.equals(userId)) {
-                throw new AccessException(String.format("Access to User id:%s for booking id:%s is denied",
-                        userId, booking.getId()));
-            }
-        } else {
-            if (!ownerId.equals(userId)) {
-                throw new NotFoundException("wrong owner");
-            }
+        if (accessForBooker && bookerId.equals(userId)) {
+            return;
         }
+        if (ownerId.equals(userId)) {
+            return;
+        }
+        throw new AccessException(String.format("Access to User id:%s for booking id:%s is denied",
+                userId, booking.getId()));
+
     }
 
     @Override
