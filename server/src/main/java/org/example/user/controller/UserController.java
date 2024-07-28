32de966278e53f1ae013500e39ec8aa9aabfb1c4
Анна Шалтайкина <>
2024-07-28 13:37:37 +0300

    sprint16

--- server/src/main/java/org/example/user/controller/UserController.java
+++ server/src/main/java/org/example/user/controller/UserController.java
@@ -6,7 +6,15 @@ import org.example.user.UserDto;
 import org.example.user.mapper.UserMapper;
 import org.example.user.service.UserService;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,6 +32,12 @@ public class UserController {
     @PutMapping
     public UserDto updateUser(@Valid @RequestBody @NotNull UserDto user) {
         return userService.update(user.getId(), user);
+    }
+
+    @PatchMapping("{id}")
+    public UserDto patchUpdateUser(@PathVariable(required = false) long id,
+                                   @Valid @RequestBody @NotNull UserDto user) {
+        return userService.update(id, user);
     }
 
     @GetMapping("{id}")
