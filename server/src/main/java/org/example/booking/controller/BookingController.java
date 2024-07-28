7092921db801e734c96cecf5035a690a1fc91fd1
Анна Шалтайкина <>
2024-07-28 14:16:03 +0300

    sprint16

--- server/src/main/java/org/example/booking/controller/BookingController.java
+++ server/src/main/java/org/example/booking/controller/BookingController.java
@@ -4,11 +4,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
-import org.example.booking.service.BookingService;
-
 import org.example.booking.InputBookingDto;
 import org.example.booking.OutputBookingDto;
-import org.springframework.data.domain.Page;
+import org.example.booking.service.BookingService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -49,18 +49,18 @@ public class BookingController {
     }
 
     @GetMapping
-    public Page<OutputBookingDto> getBookingsOfBooker(@RequestHeader(USER_ID_IN_HEADER) Long userId,
+    public List<OutputBookingDto> getBookingsOfBooker(@RequestHeader(USER_ID_IN_HEADER) Long userId,
                                                       @RequestParam(required = false, defaultValue = "ALL") String state,
                                                       @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                                       @RequestParam(defaultValue = "30") @Positive int size) {
-        return bookingService.getBookingsOfBooker(state, userId, from, size);
+        return bookingService.getBookingsOfBooker(state, userId, from, size).getContent();
     }
 
     @GetMapping("/owner")
-    public Page<OutputBookingDto> getBookingsOfOwner(@RequestHeader(USER_ID_IN_HEADER) Long userId,
+    public List<OutputBookingDto> getBookingsOfOwner(@RequestHeader(USER_ID_IN_HEADER) Long userId,
                                                      @RequestParam(defaultValue = "ALL") String state,
                                                      @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                                      @RequestParam(defaultValue = "20") @Positive int size) {
-        return bookingService.getBookingsOfOwner(state, userId, from, size);
+        return bookingService.getBookingsOfOwner(state, userId, from, size).getContent();
     }
 }
